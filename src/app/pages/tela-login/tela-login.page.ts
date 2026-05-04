e2cf25f5063a8a67d119edb3592b2f0f4d37b3a3
Caikke <caike14@gmail.com>
2026-05-04 19:37:49 -0300

    feat: implementa lógica de login e fluxo de redirecionamento

--- src/app/pages/tela-login/tela-login.page.ts
+++ src/app/pages/tela-login/tela-login.page.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { Auth, signInWithEmailAndPassword } from '@angular/fire/auth';
-import { UsuarioService } from './../../services/usuarios';
+import { AuthService } from '../../services/auth';
 
 @Component({
   selector: 'app-tela-login',
@@ -18,48 +17,25 @@ export class TelaLoginPage {
 
   constructor(
     private router: Router,
-    private auth: Auth,
-    private usuarioService: UsuarioService 
-  ) {}
+    private authService: AuthService
+  ) { }
 
   async login() {
-
-    if (!this.email || !this.senha) {
-      // obrigatório se não colocar nada
-      this.erro = 'Preencha todos os campos!';
+    if(!this.email || !this.senha) {
+      this.erro = 'Preencha todos os campos';
       return;
     }
 
-    this.erro = ''; // Limpa o erro antes de tentar o login real
-
     try {
-      // Função: Logar no Firebase
-      const res = await signInWithEmailAndPassword(this.auth, this.email, this.senha);
-
-      console.log('Logado com sucesso:', res.user);
-
-      // --- PARTE NOVA: VERIFICAR QUEM É O USUÁRIO ---
-      this.usuarioService.getUsuarioLogado().subscribe((dados: any) => {
-        
-        if (dados.status === 'desativado') {
-          this.erro = 'Sua conta está desativada!';
-          return;
-        }
-
-        // Se for ADM, vai para a tela de gerenciar usuários
-        if (dados.perfil === 'adm') {
-          this.router.navigate(['/usuarios']);
-        } else {
-          // redireciona para a tela de sugestões (usuário comum)
-          this.router.navigate(['/sugestoes']);
-        }
-      });
-      // ----------------------------------------------
-
+      await this.authService.login(this.email, this.senha);
+      this.router.navigate(['/sugestoes']);
     } catch (e: any) {
-      // se a senha ou email estiverem errados
-      console.error(e);
-      this.erro = 'Email ou senha inválidos!';
+      this.erro = 'Email ou senha inválidos!'
     }
   }
+
+  irParaRegistrar(){
+    this.router.navigate(['/registrar']);
+  }
+
 }
