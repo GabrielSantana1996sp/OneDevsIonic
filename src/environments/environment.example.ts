30ef557bc32009ca69d7786528a551e48a4a8fe5
Aluno <e023aluno@outlook.com>
2026-05-06 20:08:08 -0300

    docs: substitui credenciais reais por placeholders no arquivo de exemplo

--- src/environments/environment.example.ts
+++ src/environments/environment.example.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-  apiKey: "AIzaSyC7XGWw9whmuZ1iTS5ZsdyPr-4O5pEOQVE",
-  authDomain: "onedevsos-66dcd.firebaseapp.com",
-  projectId: "onedevsos-66dcd",
-  storageBucket: "onedevsos-66dcd.firebasestorage.app",
-  messagingSenderId: "54210837509",
-  appId: "1:54210837509:web:64e0263061caaca6f7706c",
-  measurementId: "G-HMKD8PQK4L"
+    apiKey: "SUA_API_KEY_AQUI",
+    authDomain: "SEU_PROJECT_ID.firebaseapp.com",
+    projectId: "SEU_PROJECT_ID",
+    storageBucket: "SEU_PROJECT_ID.firebasestorage.app",
+    messagingSenderId: "SEU_SENDER_ID",
+    appId: "SEU_APP_ID",
+    measurementId: "SEU_MEASUREMENT_ID"
   }
 };
