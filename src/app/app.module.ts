325f9e402a4d951d8773cfcbae33680ebc365fe5
aluno <e023aluno@outlook.com>
2026-05-05 22:04:30 -0300

    Atualizações da tela de login, cadastro e sugestões

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore} from '@angular/fire/firestore'
 import { provideAuth, getAuth } from '@angular/fire/auth';
 
-import { environment } from '../environments/environment'
+import { environment } from '../environments/environment.example'
 
 @NgModule({
   declarations: [AppComponent],
--- src/environments/environment.example.ts
+++ src/environments/environment.example.ts
@@ -1,25 +1,12 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "SUA_API_KEY_AQUI",
-    authDomain: "seu-projeto.firebaseapp.com",
-    projectId: "seu-projeto",
-    storageBucket: "seu-projeto.appspot.com",
-    messagingSenderId: "1234567890",
-    appId: "1:1234567890:web:abcdef123456",
-    measurementId: "G-ABCDEF123"
+  apiKey: "AIzaSyC7XGWw9whmuZ1iTS5ZsdyPr-4O5pEOQVE",
+  authDomain: "onedevsos-66dcd.firebaseapp.com",
+  projectId: "onedevsos-66dcd",
+  storageBucket: "onedevsos-66dcd.firebasestorage.app",
+  messagingSenderId: "54210837509",
+  appId: "1:54210837509:web:64e0263061caaca6f7706c",
+  measurementId: "G-HMKD8PQK4L"
   }
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
